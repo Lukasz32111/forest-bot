31bb7abd2803477a6be67563367bb97395bf5bab
Lukasz32111 <123389209+Lukasz32111@users.noreply.github.com>
2026-01-29 15:31:22 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,25 +6,6 @@ PREFIX = '8'
 
 INTENTS = discord.Intents.default()
 INTENTS.message_content = True
-INTENTS.voice_states = True
 INTENTS.guilds = True
 INTENTS.reactions = True
-INTENTS.guild_reactions = True
 INTENTS.members = True
-
-YTDL_FORMAT_OPTIONS = {
-    'format': 'bestaudio/best',
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'cookiefile': 'cookies.txt',
-}
-
-FFMPEG_OPTIONS = {
-    'options': '-vn',
-    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
-}
-
-MAX_HISTORY = 10
